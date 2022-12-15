a67d1c04eea6726a58494334d7acd665b8f0fd60
Bearofbusines <61521224+Bearofbusines@users.noreply.github.com>
2022-12-14 19:20:10 -0500

    added new levels and a textbox sprite and a boss fight as well as updated sprites

--- platformer.js
+++ platformer.js
@@ -33,7 +33,7 @@
             if ((request.readyState == 4) && (request.status == 200))
                 onsuccess(request);
         }
-        request.open("GET", url, true);
+        request.open("GET", url, true);request.open("GET", url, true);
         request.send();
     }
 
@@ -50,7 +50,8 @@
 
     var MAP = { tw: 64, th: 48 },
         WIN = false,
-        level = 1,
+        level = 0,
+        levelplus = false,
         TILE = 32,
         METER = TILE,
         GRAVITY = 9.8 * 6, // default (exagerated) gravity
@@ -61,7 +62,7 @@
         IMPULSE = 25,    // default player jump impulse
         COLOR = { BLACK: '#000000', YELLOW: '#ECD078', BRICK: '#D95B43', PINK: '#C02942', PURPLE: '#542437', GREY: '#333', SLATE: '#53777A', GOLD: 'gold', GREEN: 'green', RED:'red', WHITE: 'white', BLUE: 'blue' },
         COLORS = [COLOR.YELLOW, COLOR.BRICK, COLOR.PINK, COLOR.PURPLE, COLOR.GREY],
-        KEY = { SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40, W: 87, A: 65, S: 83, D: 68 };
+        KEY = { SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40, W: 87, A: 65, S: 83, D: 68, BackSlash: 220 };
 
     var fps = 60,
         step = 1 / fps,
@@ -73,11 +74,12 @@
         
         player = {},
         monsters = [],
+        boss = [],
         treasure = [],
         cells = [],
         goal = {},
         clock = {},
-        text = [];
+        ttext = [];
 
     img.src = './tiles.png';
     var t2p = function (t) { return t * TILE; },//tile to pixel
@@ -95,15 +97,46 @@
             case KEY.A: player.left = down; ev.preventDefault(); return false;
             case KEY.D: player.right = down; ev.preventDefault(); return false;
             case KEY.SPACE: player.jump = down; ev.preventDefault(); return false;
+            case KEY.BackSlash: if(!levelplus && !down){levelplus = true; WIN=true} ev.preventDefault(); return false;
         }
     }
 
     function update(dt) {
+        updateBoss(dt);
         updatePlayer(dt);
         updateMonsters(dt);
         checkTreasure();
         checkGoal()
         checkTime();
+        if(!(boss==null)){//have to but it here or it will not work idk
+            if (!boss.dead) {
+                updateBoss(boss, dt);
+                if (overlap(player.x, player.y, TILE, TILE, boss.x, boss.y, TILE*4, TILE*4)) {
+                    if(!boss.stepped){
+                        console.log("overlapping");
+                        if ((player.dy > 0) && (boss.y - player.y > TILE / 4)){
+                            if(boss.HP > 0){
+                                boss.HP--;
+                                boss.stepped == true;
+                            }else{
+                                killMonster(boss);
+                                
+                            }
+                            player.dy = -IMPULSE*25;
+                        }
+                        else{
+                            killPlayer(player);
+                            clock.clockrunning = false;
+                        }
+                    }
+                }else{
+                    console.log("not overlapping");
+                    boss.stepped = false;
+                }
+            }else{
+
+            }
+        }
     }
 
     function updatePlayer(dt) {
@@ -130,6 +163,28 @@
             }
         }
     }
+
+/*     function updateBoss(dt) {
+        console.log(JSON.stringify(boss) + " " + boss.dead );
+        if (!boss.dead && !(boss==null)) {
+            updateBoss(boss, dt);
+            if (overlap(player.x, player.y, TILE, TILE, boss.x, boss.y, TILE*4, TILE*4) && boss.stepped == false) {
+                if ((player.dy > 0) && (boss.y - player.y > TILE / 4)){
+                    if(boss.hp > 0){
+                        boss.hp--;
+                        boss.stepped == true;
+                    }else{
+                        killMonster(boss);
+                        boss.stepped == false;
+                    }
+                    player.dy = -IMPULSE*25;
+                }
+                else{
+                    killPlayer(player);
+                }
+            }
+        }
+    } */
 
     function checkTime(){
         if(clock.time <= 0){
@@ -159,6 +214,7 @@
         player.x = player.start.x;
         player.y = player.start.y;
         player.dx = player.dy = 0;
+        clock.audio.pause();
         startlvl();
     }
 
@@ -310,9 +366,170 @@
             }
         }
 
+        if (entity.boss) {
+            if (entity.left && (cell || !celldown)) {
+                entity.left = false;
+                entity.right = true;
+            }
+            else if (entity.right && (cellright || !celldiag)) {
+                entity.right = false;
+                entity.left = true;
+            }
+        }
+
         entity.falling = !(celldown || (nx && celldiag));
 
     }
+
+    function updateBoss(entity, dt) {
+        var wasleft = entity.dx < 0,
+            wasright = entity.dx > 0,
+            falling = entity.falling,
+            friction = entity.friction * (falling ? 0.5 : 1),
+            accel = entity.accel * (falling ? 0.5 : 1);
+
+        entity.ddx = 0;
+        entity.ddy = entity.gravity;
+
+        var tx = p2t(entity.x),
+            ty = p2t(entity.y),
+            nx = entity.x % TILE,
+            ny = entity.y % TILE,
+            cell = tcell(tx, ty),
+            cellright = tcell(tx + 4, ty),
+            cellleft = tcell(tx - 1, ty),
+            cellrightex = ccell(entity.x + 3 + TILE, entity.y),//ex means exact
+            cellleftex = ccell(entity.x - 3, entity.y),
+            celldown = tcell(tx, ty + 1*4),
+            celldiag = tcell(tx + 1, ty + 1),
+            slidingL = /* entity.left  && */ cellleftex,
+            slidingR = /* entity.right && */ cellrightex;
+
+        if (entity.left) {//move left
+            entity.ddx = entity.ddx - accel;
+            if (cellleftex && entity.dy > 0) {//sliding motion
+                entity.dy = friction / 20;
+            }
+        } else if (wasleft) {
+            entity.ddx = entity.ddx + friction;
+        }
+
+        if (entity.right) {//move right
+            entity.ddx = entity.ddx + accel;
+            if (cellrightex && entity.dy > 0) {//sliding motion
+                entity.dy = friction / 20;
+            }
+        }
+        else if (wasright) {
+            entity.ddx = entity.ddx - friction;
+        }
+
+
+
+        if (entity.jump && !entity.jumping && !falling) {
+            entity.dy = /* entity.ddy */ - entity.impulse; // an instant big force impulse
+            entity.jumping = true;
+            entity.justJumped = true;
+        } else if (entity.jump && falling && cellrightex && !entity.wallJumpingR && entity.jumping && slidingR && !entity.justJumped) {//on right wall
+            entity.dy = /*entity.ddy*/ - entity.impulse * 0.7;
+            entity.ddx = /*entity.ddx*/ - entity.impulse * 700;
+            entity.wallJumpingR = true;
+            entity.jumping = true;
+            entity.justJumped = true;
+            //console.log("wall jump right");
+        } else if (entity.jump && falling && cellleftex  && !entity.wallJumpingL && entity.jumping && slidingL && !entity.justJumped) {//on left wall
+            entity.dy = /*entity.ddy*/ - entity.impulse * 0.7;
+            entity.ddx = /*entity.ddx +*/ entity.impulse * 700;
+            entity.dx = /*entity.ddx +*/ entity.impulse;
+            entity.wallJumpingL = true;
+            entity.jumping = true;
+            entity.justJumped = true;
+            //console.log("wall jump left");
+        }
+
+        if (!entity.jump) {
+            entity.justJumped = false;
+        }
+
+        if(falling && !cellright && entity.jumping || !entity.jump){
+            entity.wallJumpingR = false;
+        }
+
+        if(falling && !cellleft  && entity.jumping || !entity.jump){
+            entity.wallJumpingL = false;
+        }
+
+        entity.x = entity.x + (dt * entity.dx);
+        entity.y = entity.y + (dt * entity.dy);
+        entity.dx = bound(entity.dx + (dt * entity.ddx), -entity.maxdx, entity.maxdx);
+        entity.dy = bound(entity.dy + (dt * entity.ddy), -entity.maxdy, entity.maxdy);
+
+        if ((wasleft && (entity.dx > 0) && (entity.dx < friction) && !entity.wallJumpingL) ||
+            (wasright && (entity.dx < 0) &&(entity.dx > -friction)&& !entity.wallJumpingR)) {
+            //console.log("this is active: " + entity.dx);
+            entity.dx = 0; // clamp at zero to prevent friction from making us jiggle side to side
+        }
+
+
+        if (entity.dy > 0) {
+            if ((celldown && !cell) ||
+                (celldiag && !cellright && nx)) {
+                entity.y = t2p(ty);
+                entity.dy = 0;
+                entity.falling = false;
+                entity.jumping = false;
+                entity.wallJumping = false;
+                ny = 0;
+            }
+        }
+        else if (entity.dy < 0) {
+            if ((cell && !celldown) ||
+                (cellright && !celldiag && nx)) {
+                entity.y = t2p(ty + 1);
+                entity.dy = 0;
+                cell = celldown;
+                cellright = celldiag;
+                ny = 0;
+            }
+        }
+
+        if (falling && (!cellleft && !cellright) && entity.wallJumping) {
+            entity.wallJumping = false;
+        }
+
+        if (entity.dx > 0) {
+            if ((cellright && !cell) ||
+                (celldiag && !celldown && ny)) {
+                entity.x = t2p(tx);
+                entity.dx = 0;
+            }
+        }
+        else if (entity.dx < 0) {
+            if ((cell && !cellright) ||
+                (celldown && !celldiag && ny)) {
+                entity.x = t2p(tx + 1);
+                entity.dx = 0;
+            }
+        }
+
+        if (entity.boss) {
+            if (entity.left && (cellleft)) {
+                console.log("left");
+                entity.left = false;
+                entity.right = true;
+            }
+            else if (entity.right && (cellright)) {
+                console.log("right");
+                entity.right = false;
+                entity.left = true;
+            }
+        }
+
+        entity.falling = !(celldown || (nx && celldiag));
+
+    }
+
+
 
     //-------------------------------------------------------------------------
     // RENDERING
@@ -324,9 +541,17 @@
         renderGoal(ctx, frame);
         renderPlayer(ctx, dt);
         renderMonsters(ctx, dt);
+        renderBoss(ctx, dt);
         renderMap(ctx);
         renderClock(ctx, dt);
-        renderText(ctx, dt)
+        renderText(ctx, dt);
+        if(!(boss==null)){//have to but it here or it will not work idk
+            if (boss.dead) {
+                ctx.fillStyle = "white";
+                ctx.font = "bold 120px Arial";
+                ctx.fillText("You Win", 500, 500);
+            }   
+        }
     }
 
     function renderMap(ctx) {
@@ -395,25 +620,54 @@
         }
     }
 
+    function renderBoss(ctx, dt) {
+        if(!(boss == null)){
+            if (!boss.dead){
+                if (boss.dx != 0) {
+                    boss.frame += boss.dx/(Math.PI*256);
+                }
+                boss.frame = boss.frame % 360;
+                //ctx.fillRect(player.x + (player.dx * dt), player.y + (player.dy * dt), TILE, TILE);
+                ctx.save(); // save current state
+                ctx.translate(boss.x + (boss.dx * dt) + TILE*2,boss.y + (boss.dy * dt)+ TILE*2);
+                ctx.rotate(boss.frame); // rotate
+                //drawFrame(ctx, 0, 3, -TILE / 2, -TILE / 2);
+                ctx.drawImage(img,
+                    64, 96, 64, 64,
+                    -TILE*2, -TILE*2, TILE*4, TILE*4,);
+                ctx.restore();
+            }
+        }
+    }
+
     function renderClock(ctx, dt) {
         ctx.fillStyle = "#928E8E";
         ctx.font = '90px Arial LightGray';
+        if(clock.clockrunning){
         var timee = new Date().valueOf();
         clock.time += (clock.timel - timee)/1000;
         clock.timel = timee
-        var text = clock.time;
-        text = text.toFixed(2);
-        ctx.fillText(text, clock.x, clock.y + 90);
+        }
+        var ctext = clock.time;
+        ctext = ctext.toFixed(2);
+        ctx.fillText(ctext, clock.x, clock.y + 90);
+        if(!clock.playing && player.x != player.start.x){
+            clock.audio = new Audio(clock.music);
+            clock.audio.volume = 0.2;
+            clock.audio.loop = true;
+            clock.audio.play(); 
+            clock.playing = true;
+        }
     }
 
     function renderText(ctx, dt) {
-        ctx.fillStyle = "#928E8E";
-        ctx.font = '90px Arial LightGray';
+        
         var n, max, t;
-        for (n = 0, max = text.length; n < max; n++) {
-            console.log(text[n].text);
-            t = text[n];
-            ctx.fillText(t.text, t.x, t.y + 90);
+        for (n = 0, max = ttext.length; n < max; n++) {
+            t = ttext[n];
+            ctx.fillStyle = t.color;
+            ctx.font = t.font;
+            ctx.fillText(t.textcon, t.x, t.y + 90);
         }
     }
 
@@ -457,10 +711,11 @@
             switch (entity.name) {
                 case "player": player = entity; break;
                 case "monster": monsters.push(entity); break;
+                case "boss": boss = entity; break;
                 case "treasure": treasure.push(entity); break;
                 case "goal": goal = entity; break;
                 case "clock": clock = entity; break;
-                case "text": text += entity; break;
+                case "textBox": ttext.push(entity); break;
             }
         }
 
@@ -470,7 +725,8 @@
     function rectifyClasses(arr) {
         var fixed = {};
         for(var i = 0; i<arr.length; i++) {
-            //eval("fixed." + arr[i].name) = arr[i].value;            //console.log("rectifyClasses: " + JSON.stringify(arr[i]));
+            //eval("fixed." + arr[i].name) = arr[i].value;            
+            //console.log("rectifyClasses: " + JSON.stringify(arr[i]));
             switch (arr[i].name) {
                 case "none":
                     fixed.ItIsVeryLate = "and i wanna kms";
@@ -505,19 +761,27 @@
                 case "time":
                     fixed.time = arr[i].value;
                     break;
-                case "text":
-                    fixed.text = arr[i].value;
+                case "textco":
+                    //console.log("text: " + arr[i].value);
+                    fixed.textco = arr[i].value;
                     break;
                 case "font":
                     fixed.font = arr[i].value;
                     break;
+                case "color":
+                    fixed.color = arr[i].value;
+                    break;
+                case "music":
+                    fixed.music = arr[i].value;
+                    break;
                 default:
-                    console.log("Unknown class: " + arr[i].name);
+                    console.log("Unknown class: " + arr[i].name.toString());
                     break;
                 //eval("fixed." + arr[i].name + " = " + arr[i].value);
             }
 
         }
+        //console.log("fixed: " + JSON.stringify(fixed) + " " + fixed.textco);
         return fixed;
     }
 
@@ -526,8 +790,19 @@
         if(!obj.hasOwnProperty("properties")){
             obj.properties = [{name:"none", value:0}];
         }
+        //console.log("setup: " + JSON.stringify(obj));
+        for(var property in obj.properties) {
+            //console.log("setup: " + property.name + " " + property.value);
+        }
         obj.properties = rectifyClasses(obj.properties);
+        
         //console.log("obj.properties: " + obj.properties);
+        /* for(var property in obj) {
+            console.log(property + "=" + obj[property]);
+        }
+        for(var property in obj.properties) {
+            console.log(property + "=" + obj[property]);
+        } */
         var entity = {};
         entity.x = obj.x;
         entity.y = obj.y;
@@ -545,6 +820,8 @@
         entity.treasure = obj.name == "treasure";
         entity.goal = obj.name == "goal";
         entity.clock = obj.name == "clock";
+        entity.text = obj.name == "text";
+        entity.boss = obj.name == "boss";
         entity.left = obj.properties.left;
         entity.right = obj.properties.right;
         entity.start = { x: obj.x, y: obj.y };
@@ -555,8 +832,15 @@
         entity.frame = 0;//it is realing rotation but i dont want to change it
         entity.time = (obj.properties.time || -1);
         entity.timel = new Date().valueOf();
-        entity.text = (obj.properties.text || "");
-        entity.font = (obj.properties.font || "90px Arial LightGray");
+        entity.textcon = (obj.properties.textco || "");
+        entity.font = (obj.properties.font || "");
+        entity.color = (obj.properties.color || COLOR.BLACK);
+        entity.stepped = false;
+        entity.music = (obj.properties.music || "");
+        entity.clockrunning = true;
+        entity.playing = false;
+        entity.audio;
+        //console.log("entity.font: " + entity.font);
         return entity;
     }
 
@@ -578,7 +862,8 @@
             update(step);
         }
         if(WIN){
-            console.log("WIN");
+            //console.log("WIN");
+            clock.audio.pause();
             level++;
             startlvl(false);
             //return;
@@ -603,9 +888,10 @@
             cells = [],
             goal = {};
             clock = {};
-            text = [];
+            ttext = [];
+            levelplus = false;
+            boss = null;
             setup(JSON.parse(req.responseText));
-
             WIN = false;
             if(first){
                 frame();
